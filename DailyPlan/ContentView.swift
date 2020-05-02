5a8bf5a11a67a39edd5613f3be9d142476e90fe6
ZDayer <zhaoyangofemail@163.com>
2020-05-02 16:31:12 +0800

    random number

--- DailyPlan/ContentView.swift
+++ DailyPlan/ContentView.swift
@@ -11,34 +11,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        NavigationView {
-            NavigationLink(destination: DailyDetail()) {
-                VStack {
-                    ClickView()
-                        .frame(width: 250, height: 250)
-                        .background(Color("bgColor"))
-                        .cornerRadius(125)
-                }
-            }
+         ClickView()
+            .frame(width: 250, height: 250)
+            .background(Color("bgColor"))
+            .cornerRadius(125)
+            .onTapGesture {
+                let random = randomDailyNumber()
+                print(random)
         }
         
-        
-        
-
+//        NavigationView {
+//            NavigationLink(destination: DailyDetail()) {
+//                VStack {
+//                    ClickView()
+//                        .frame(width: 250, height: 250)
+//                        .background(Color("bgColor"))
+//                        .cornerRadius(125)
+//                }
+//            }
+//        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        
     }
 }
 
 struct ClickView: View {
+    var textContent = "选取今日数字"
     var body: some View {
-        Text("选取今日数字")
+        Text(textContent)
             .foregroundColor(.white)
             .font(.largeTitle)
     }
+}
+
+func randomDailyNumber() -> Int {
+    let number = arc4random()%365+1
+    return Int(number)
 }
