b343e392d24394acce2e4e87ec03ffe36362af22
zhaoyang <zhaoy_305@bankcomm.com>
2020-05-11 13:48:00 +0800

    测试SnapKit

--- DailyPlan/HomeViewController.swift
+++ DailyPlan/HomeViewController.swift
@@ -15,13 +15,29 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let subView = UIView()
-        subView.backgroundColor = UIColor.red
-        self.view.addSubview(subView)
-        subView.snp.makeConstraints { (ConstraintMaker) in
-            ConstraintMaker.width.height.equalTo(100)
-            ConstraintMaker.center.equalToSuperview()
+        self.initSubviews()
+    }
+    
+    func initSubviews() {
+        
+        let clickView = UIView()
+        clickView.backgroundColor = .blue
+        clickView.layer.cornerRadius = 125
+        self.view.addSubview(clickView)
+        clickView.snp.makeConstraints { (make) in
+            make.width.height.equalTo(250)
+            make.center.equalToSuperview()
         }
+        
+        let lbl = UILabel()
+        clickView.addSubview(lbl)
+        lbl.text = "选择今日数字"
+        lbl.font = .boldSystemFont(ofSize: 20)
+        lbl.textColor = .white
+        lbl.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
+        
     }
 
 }
